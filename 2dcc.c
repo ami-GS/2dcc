0765b95eae749d2b0082363982dc669d052cad44
ami-GS <1991.daiki@gmail.com>
2019-08-31 16:14:09 -0700

    support multi byte token

--- 2dcc.c
+++ 2dcc.c
@@ -18,6 +18,7 @@ struct Token {
     Token *next;
     int val;
     char *str;
+    int len;
 };
 
 Token *token; // TODO: will be local
@@ -160,10 +161,13 @@ bool at_eof() {
     return token->kind == TK_EOF;
 }
 
-Token* new_token(TokenKind kind, Token *cur, char* str) {
+Token* new_token(TokenKind kind, Token *cur, char* str, int len) {
     Token *tok = calloc(1, sizeof(Token));
     tok->kind = kind;
+    tok->str = calloc(len, sizeof(char));
+    memcpy(tok->str, str, sizeof(char) * len);
     tok->str = str;
+    tok->len = len;
     cur->next = tok;
     return tok;
 }
@@ -179,21 +183,30 @@ Token *tokenize(char *p) {
             continue;
         }
 
-        if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
-            cur = new_token(TK_RESERVED, cur, p++);
+        if (strncmp(p, "==", 2) == 0 || strncmp(p, "!=", 2) == 0) {
+            cur = new_token(TK_RESERVED, cur, p, 2);
+            p += 2;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-' || *p == '*' ||
+         *p == '/' || *p == '(' || *p == ')') {
+            cur = new_token(TK_RESERVED, cur, p++, 1);
             continue;
         }
 
         if (isdigit(*p)) {
-            cur = new_token(TK_NUM, cur, p);
+            cur = new_token(TK_NUM, cur, p, 0);
+            char* pTmp = p;
             cur->val = strtol(p, &p, 10);
+            cur->len = p - pTmp;
             continue;
         }
 
         error("failed to tokenize");
     }
 
-    new_token(TK_EOF, cur, p);
+    new_token(TK_EOF, cur, p, 0);
     return head.next;
 }
 
