c443fdc0996c6311d8dfcaf27297e1f838ee4a47
ami-GS <1991.daiki@gmail.com>
2019-12-29 15:04:26 -0800

    support any test

--- tests.sh
+++ tests.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
-for file in ./tests/*.c; do
-    ./2dcc ${file} > tmp.s
-    if [[ "$file" == *"global"* ]]; then
+assemble() {
+    if [[ "$1" == *"global"* ]]; then
         # need to remove static
         gcc -static -g -O0 -o tmp tmp.s
     else
         gcc -g -O0 -o tmp tmp.s
     fi
-    ./tmp
-    actual=$?
-    if [ ${actual} == 100 ]; then
-        echo -e "${file}:\t SUCCESS => 100"
+}
+
+validate() {
+    file=$1
+    actual=$2
+    expected=$3
+    if [ ${actual} == ${expected} ]; then
+        echo -e "${file}:\t SUCCESS => ${expected}"
     else
         echo -e "${file}:\t FAILED => ${actual}"
         exit 1
     fi
-done
+}
+
+if [[ $# == 0 ]]; then
+    for file in ./tests/*.c; do
+        ./2dcc ${file} > tmp.s
+        assemble $file
+        ./tmp
+        validate $file $? 100
+    done
+else
+    for file in $* ; do
+        ./2dcc ${file} > tmp.s
+        assemble ${file}
+        ./tmp
+        validate ${file} $? 100
+    done
+fi
